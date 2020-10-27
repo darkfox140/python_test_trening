8cc4b19d6ae6679d7481e8a1f9f4d2f4f67ce61f
Андрей Мальцев <malcev223@gmail.com>
2020-10-27 15:30:39 +0300

    Выделены вспомогательные методы и классы.

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from group import Group
 import unittest
 
 
@@ -13,7 +14,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(browser)
         self.login(browser, username="admin", password="secret")
         self.open_groups_page(browser)
-        self.create_group(browser, name="qwerty", header="qazxsw", footer="asdf")
+        self.create_group(browser, Group(name="qwerty", header="qazxsw", footer="asdf"))
         self.return_to_groups_page(browser)
         self.logout(browser)
 
@@ -22,29 +23,29 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(browser)
         self.login(browser, username="admin", password="secret")
         self.open_groups_page(browser)
-        self.create_group(browser, name="", header="", footer="")
+        self.create_group(browser, Group(name="", header="", footer=""))
         self.return_to_groups_page(browser)
         self.logout(browser)
 
     def logout(self, browser):
         browser.find_element(By.LINK_TEXT, "Logout").click()
 
-    def return_to_groups_page(self, browser):
+    def return_to_groups_page(self, browser,):
         browser.find_element(By.LINK_TEXT, "group page").click()
 
-    def create_group(self, browser, name, header, footer):
+    def create_group(self, browser, group):
         # init group creation
         browser.find_element(By.NAME, "new").click()
         # fill group form
         browser.find_element(By.NAME, "group_name").click()
         browser.find_element(By.NAME, "group_name").clear()
-        browser.find_element(By.NAME, "group_name").send_keys(name)
+        browser.find_element(By.NAME, "group_name").send_keys(group.name)
         browser.find_element(By.NAME, "group_header").click()
         browser.find_element(By.NAME, "group_header").clear()
-        browser.find_element(By.NAME, "group_header").send_keys(header)
+        browser.find_element(By.NAME, "group_header").send_keys(group.header)
         browser.find_element(By.NAME, "group_footer").click()
         browser.find_element(By.NAME, "group_footer").clear()
-        browser.find_element(By.NAME, "group_footer").send_keys(footer)
+        browser.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
         browser.find_element(By.NAME, "submit").click()
 
