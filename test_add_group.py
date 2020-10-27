c86f9bf553719f82e02fd655a9f7d19c167fdffc
Андрей Мальцев <malcev223@gmail.com>
2020-10-27 14:35:30 +0300

    Созданны вспомогательные методы для улучшения читаемости кода

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
 
@@ -12,15 +10,23 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         browser = self.browser
-        browser.get("http://localhost/addressbook/index.php")
-        browser.find_element(By.NAME, "user").click()
-        browser.find_element(By.NAME, "user").clear()
-        browser.find_element(By.NAME, "user").send_keys("admin")
-        browser.find_element(By.NAME, "pass").clear()
-        browser.find_element(By.NAME, "pass").send_keys("secret")
-        browser.find_element(By.XPATH, "//input[@value='Login']").click()
-        browser.find_element(By.LINK_TEXT, "groups").click()
+        self.open_home_page(browser)
+        self.login(browser)
+        self.open_groups_page(browser)
+        self.create_group(browser)
+        self.return_to_groups_page(browser)
+        self.logout(browser)
+
+    def logout(self, browser):
+        browser.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups_page(self, browser):
+        browser.find_element(By.LINK_TEXT, "group page").click()
+
+    def create_group(self, browser):
+        # init group creation
         browser.find_element(By.NAME, "new").click()
+        # fill group form
         browser.find_element(By.NAME, "group_name").click()
         browser.find_element(By.NAME, "group_name").clear()
         browser.find_element(By.NAME, "group_name").send_keys("qwerty")
@@ -30,23 +36,22 @@ class TestAddGroup(unittest.TestCase):
         browser.find_element(By.NAME, "group_footer").click()
         browser.find_element(By.NAME, "group_footer").clear()
         browser.find_element(By.NAME, "group_footer").send_keys("asdf")
+        # submit group creation
         browser.find_element(By.NAME, "submit").click()
-        browser.find_element(By.LINK_TEXT, "group page").click()
-        browser.find_element(By.LINK_TEXT, "Logout").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
+    def open_groups_page(self, browser):
+        browser.find_element(By.LINK_TEXT, "groups").click()
 
-    def is_alert_present(self):
-        try:
-            self.browser.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
+    def login(self, browser):
+        browser.find_element(By.NAME, "user").click()
+        browser.find_element(By.NAME, "user").clear()
+        browser.find_element(By.NAME, "user").send_keys("admin")
+        browser.find_element(By.NAME, "pass").clear()
+        browser.find_element(By.NAME, "pass").send_keys("secret")
+        browser.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, browser):
+        browser.get("http://localhost/addressbook/index.php")
 
     def tearDown(self):
         self.browser.quit()
