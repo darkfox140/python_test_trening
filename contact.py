f67feaf9f9bace9253526b3a5cd68204a94f06f6
Андрей Мальцев <malcev223@gmail.com>
2020-10-28 14:35:13 +0300

    Добавленн новый файл.

---
+++ contact.py
@@ -0,0 +1,32 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nick_name, title, company, address, home_phone,
+                     mobile_phone, work_phone, fax, email1, email2, email3, homepage, bday, bmoth, byears, aday, amonth,
+                     ayear, address2, phone2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmoth = bmoth
+        self.byears = byears
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notesotes = notes
+
+
+
