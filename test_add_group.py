f69c95213dbc3a0d8b04fcf13c6d3931531e6c3d
Андрей Мальцев <malcev223@gmail.com>
2020-10-28 16:21:43 +0300

    Переделал всё чтоб работало. Теперь тест запускаеться и проходиться

--- test_add_group.py
+++ test_add_group.py
@@ -38,13 +38,10 @@ class TestAddGroup(unittest.TestCase):
         # init group creation
         browser.find_element(By.NAME, "new").click()
         # fill group form
-        browser.find_element(By.NAME, "group_name").click()
         browser.find_element(By.NAME, "group_name").clear()
         browser.find_element(By.NAME, "group_name").send_keys(group.name)
-        browser.find_element(By.NAME, "group_header").click()
         browser.find_element(By.NAME, "group_header").clear()
         browser.find_element(By.NAME, "group_header").send_keys(group.header)
-        browser.find_element(By.NAME, "group_footer").click()
         browser.find_element(By.NAME, "group_footer").clear()
         browser.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
