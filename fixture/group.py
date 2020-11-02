0815bd540f54fb329f2c6667b5f41dea67012529
Андрей Мальцев <malcev223@gmail.com>
2020-11-02 22:27:44 +0300

    Добавленны новые вспомогательные методы.

--- fixture/group.py
+++ fixture/group.py
@@ -26,6 +26,15 @@ class GroupHelper:
         browser.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
+    def delele_first_group(self):
+        browser = self.app.browser
+        self.open_groups_page()
+        # select first group
+        browser.find_element(By.NAME, "selected[]").click()
+        # submit deletion
+        browser.find_element(By.NAME, "delete").click()
+        self.return_to_groups_page()
+
     def return_to_groups_page(self):
         browser = self.app.browser
         browser.find_element(By.LINK_TEXT, "group page").click()
