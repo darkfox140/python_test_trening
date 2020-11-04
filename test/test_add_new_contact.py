1d8300a8418f1e49721de157cf6de2972aee8181
Андрей Мальцев <malcev223@gmail.com>
2020-11-04 20:59:45 +0300

    Изменения в тесте.

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -24,11 +24,14 @@ def test_add_empty_contact(app):
 def test_modification_empty_contact(app):
     app.session.login(username="admin", password="secret")
     app.cotact.modification_empty_contact((NewContact(last_name="Ivan", middle_name="Petr", first_name="Kulik",
-                                             nick_name="red23",tittle="test", company="testcompany", address1="Omsk",
-                                             home_phone="83812*******", mobile_phone="89158******",
-                                             work_phone="83812*******", fax="No", email1="testemail1@facegmail.com",
-                                             email2="testemail2@facegmail.com", email3="testemail3@facegmail.com",
-                                             homepage="testhomepage.ru", bday="25", bmonth="September", byear="1989",
-                                             aday="07", amonth="May", ayear="2008", address2="Omsk, Lenina 2",
-                                             phone2="Phone2test", notes="Test Omsk information")))
+                                                      nick_name="red23",tittle="test", company="testcompany",
+                                                      address1="Omsk", home_phone="83812*******",
+                                                      mobile_phone="89158******", work_phone="83812*******", fax="No",
+                                                      email1="testemail1@facegmail.com",
+                                                      email2="testemail2@facegmail.com",
+                                                      email3="testemail3@facegmail.com",
+                                                      homepage="facehomepage.ru", bday="8", bmonth="September",
+                                                      byear="1989", aday="7", amonth="May", ayear="2008",
+                                                      address2="Omsk, Lenina 2", phone2="Phone2test",
+                                                      notes="Test Omsk information")))
     app.session.logout()
