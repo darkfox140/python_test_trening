59426fe2502afc555daf3b7afd1a269144a901ae
Андрей Мальцев <malcev223@gmail.com>
2020-11-06 22:55:08 +0300

    Изменена функции login и logout.

--- conftest.py
+++ conftest.py
@@ -1,9 +1,26 @@
 from fixture.application import Applicatin
 import pytest
 
+fixture = None
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture
 def app(request):
-    fixture = Applicatin()
-    request.addfinalizer(fixture.destroy)
+    global fixture
+    if fixture is None:
+        fixture = Applicatin()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Applicatin()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
     return fixture
--- fixture/application.py
+++ fixture/application.py
@@ -13,6 +13,14 @@ class Applicatin:
         self.group = GroupHelper(self)
         self.cotact = ContactHelper(self)
 
+    def is_valid(self):
+        try:
+
+            self.browser.current_url
+            return True
+        except:
+            return False
+
     def open_home_page(self):
         browser = self.browser
         browser.get("http://localhost/addressbook/index.php")
