14b73ddeb8edec5572f2fbd14dcbd5d142817c1d
fox140 <malcev223@gmail.com>
2020-11-10 20:12:07 +0300

    Изменены тесты.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,8 +2,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="qwerty", header="qazxsw", footer="asdf"))
+    app.group.create_group(Group(name="qwerty", header="qazxsw", footer="asdf"))
 
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create_group(Group(name="", header="", footer=""))
