b5cb0ec138cd3a6051eac19522281019ccb08405
fox140 <malcev223@gmail.com>
2020-11-10 20:23:16 +0300

    Добавленны новые тесты.

--- test/test_modification_first_contact.py
+++ test/test_modification_first_contact.py
@@ -2,4 +2,12 @@ from model.contact import NewContact
 
 
 def test_modification_first_contact_address1(app):
-    app.contact.modification_first_contact((NewContact(address1="Omsk, Lenina 2")))
+    app.contact.modification_first_contact((NewContact(address1="Омск, Ленина 2")))
+
+
+def test_modification_first_contact_birthday(app):
+    app.contact.modification_first_contact((NewContact(bday="24", bmonth="March", byear="1996")))
+
+
+def test_modification_first_contact_birthday_anniversary(app):
+    app.contact.modification_first_contact((NewContact(aday="12", amonth="July", ayear="2008")))
