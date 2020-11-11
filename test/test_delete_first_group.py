e6af43fc66eaa15522709236b7407b749aed97d2
fox140 <malcev223@gmail.com>
2020-11-11 14:45:37 +0300

    Повышенна интелектуальнось тестов.

--- test/test_delete_first_group.py
+++ test/test_delete_first_group.py
@@ -1,4 +1,7 @@
+from model.group import Group
 
 
 def test_delete_group(app):
+    if app.group.count() == 0:
+        app.group.create_group(Group(footer="test footer"))
     app.group.delete_first_group()
--- test/test_modification_first_group.py
+++ test/test_modification_first_group.py
@@ -2,8 +2,12 @@ from model.group import Group
 
 
 def test_modification_group_name(app):
+    if app.group.count() == 0:
+        app.group.create_group(Group(name="test name"))
     app.group.modification_first_group(Group(name="Клуб"))
 
 
 def test_modification_group_header(app):
+    if app.group.count() == 0:
+        app.group.create_group(Group(header="test header"))
     app.group.modification_first_group(Group(header="Участники"))
