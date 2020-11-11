027901e8fb8d11dc261c9b5650c0ac202e06cd77
fox140 <malcev223@gmail.com>
2020-11-11 15:06:18 +0300

    Повышенна интелектуальность тестов.

--- test/test_delete_first_contact.py
+++ test/test_delete_first_contact.py
@@ -1,4 +1,7 @@
+from model.contact import NewContact
 
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(NewContact(nick_name="Железобетон"))
     app.contact.delete_first_contact()
--- test/test_modification_first_contact.py
+++ test/test_modification_first_contact.py
@@ -2,12 +2,18 @@ from model.contact import NewContact
 
 
 def test_modification_first_contact_address1(app):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(NewContact(address1="Тест адресса"))
     app.contact.modification_first_contact((NewContact(address1="Омск, Ленина 2")))
 
 
 def test_modification_first_contact_birthday(app):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(NewContact(bday="01", bmonth="January", byear="2000"))
     app.contact.modification_first_contact((NewContact(bday="24", bmonth="March", byear="1996")))
 
 
 def test_modification_first_contact_birthday_anniversary(app):
-    app.contact.modification_first_contact((NewContact(aday="12", amonth="July", ayear="2008")))
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(NewContact(bday="01", bmonth="January", byear="2000"))
+    app.contact.modification_first_contact((NewContact(aday="22", amonth="February", ayear="19988")))
