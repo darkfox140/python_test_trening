828ae960379957411e983fdcbe2b7d04b71f8f0c
fox140 <malcev223@gmail.com>
2020-11-13 21:14:44 +0300

    Добавленны проверки в тест.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,9 +2,11 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_group(Group(name="qwerty", header="qazxsw", footer="asdf"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
     app.group.create_group(Group(name="", header="", footer=""))
-
