75c40a7df7794dc058fca8910212cc142e37cae4
fox140 <malcev223@gmail.com>
2020-11-15 21:00:32 +0300

    Замена селектора на более осознанный.

--- fixture/contact.py
+++ fixture/contact.py
@@ -117,7 +117,7 @@ class ContactHelper:
         contacts = []
         for element1 in browser.find_elements(By.XPATH, "//tbody/tr/td[3]"):
             text1 = element1.text
-            elements2 = browser.find_element(By.XPATH, "//tbody/tr/td[2]")
+            elements2 = browser.find_element(By.XPATH, "//tbody/tr[2]/td[2]")
             text2 = elements2.text
             id = browser.find_element(By.NAME, "selected[]").get_attribute("value")
             contacts.append(NewContact(first_name=text1, last_name=text2, id=id))
