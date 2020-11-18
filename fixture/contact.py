af20861500678a505ef53071773dd957b4355265
fox140 <malcev223@gmail.com>
2020-11-18 18:00:02 +0300

    Изменения в методе modification_contact_by_index. Теперь тесты не падают.

--- fixture/contact.py
+++ fixture/contact.py
@@ -81,7 +81,7 @@ class ContactHelper:
         browser = self.app.browser
         self.open_home()
         contact_elem = browser.find_elements(By.NAME, "entry")
-        cells = contact_elem[index].find_elements(By.TAG_NAME, "td")
+        cells = contact_elem[index+1].find_elements(By.XPATH, "//td")
         cells[7].find_element(By.CSS_SELECTOR, "#maintable a img").click()
         self.fill_contact_form(new_contact_form)
         browser.find_element(By.XPATH, "//form[1]/input[22]").click()
