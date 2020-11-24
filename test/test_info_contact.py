24d1f87ca025957b2094ddf4b45d3d954e9a2645
fox140 <malcev223@gmail.com>
2020-11-24 18:55:36 +0300

    Добавленн новый тест и проверки в него.

---
+++ test/test_info_contact.py
@@ -0,0 +1,42 @@
+from model.contact import NewContact
+from random import randrange
+import re
+
+
+def test_phones_on_home_page(app):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(NewContact(first_name="Victor", middle_name="Ivan", last_name="Petrov",
+                                              nick_name="Fox140", tittle="test", company="testcompany", address1="Moscow",
+                                              home_phone="8(495)2154536", mobile_phone="+79168956384",
+                                              work_phone="8-499-9576135", fax="No", email1="testemail1@facegmail.com",
+                                              email2="testemail2@facegmail.com", email3="testemail3@facegmail.com",
+                                              homepage="testhomepage.ru", bday="14", bmonth="August", byear="1987",
+                                              aday="24", amonth="October", ayear="2014", address2="Moscow, Lenina 2",
+                                              phone2="8 456 9535956", notes="Test Moscow information"))
+    old_contact = app.contact.get_contact_list()
+    index = randrange(len(old_contact))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.first_name == contact_from_edit_page.first_name
+    assert contact_from_home_page.last_name == contact_from_edit_page.last_name
+    assert contact_from_home_page.address1 == contact_from_edit_page.address1
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
+
+
+def clear(s):
+    return re.sub('[() -]', "", s)
+
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None, [contact.home_phone, contact.mobile_phone,
+                                                                 contact.work_phone, contact.phone2]))))
+
+
+def merge_email_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email1, contact.email2, contact.email3]))))
