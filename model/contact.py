2da46a5dc1e02ba4ac3d9c185be43fd5e397479c
fox140 <malcev223@gmail.com>
2020-11-25 20:21:19 +0300

    Дополнения в функциях

--- model/contact.py
+++ model/contact.py
@@ -36,7 +36,8 @@ class NewContact:
         self.all_email_from_home_page = all_email_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.last_name, self.first_name)
+        return "%s:%s:%s:%s:%s:%s" % (self.id, self.last_name, self.first_name,
+                                      self.middle_name, self.company, self.address1)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name \
