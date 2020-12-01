91ec1a6e2317f326db80fe31a80920d3ed4c48df
fox140 <malcev223@gmail.com>
2020-12-01 15:41:30 +0300

    Созданна динамическая генерация тестов.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,8 @@
 from model.group import Group
-import pytest
-# from data.add_group import constant as test_date
-from data.add_group import test_date
 
 
-@pytest.mark.parametrize("group", test_date, ids=[repr(x) for x in test_date])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create_group(group)
     assert len(old_groups) + 1 == app.group.count()
