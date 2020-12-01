f76f02f51f9fb718751642e1431543f3443771ae
fox140 <malcev223@gmail.com>
2020-12-01 14:14:02 +0300

    Изменения в файле.

--- data/add_group.py
+++ data/add_group.py
@@ -2,6 +2,7 @@ from model.group import Group
 import random
 import string
 
+
 constant = [
     Group(name="name1", header="header1", footer="footer1"),
     Group(name="name2", header="header2", footer="footer2")
@@ -16,4 +17,3 @@ def random_string(prefix, max_len):
 test_date = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
     for i in range(5)]
-
