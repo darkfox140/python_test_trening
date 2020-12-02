2a2894971a10cd5719db133fc571590972f2336d
fox140 <malcev223@gmail.com>
2020-12-02 15:26:02 +0300

    Реализованна функция pytest_generate_tests для групп и контактов.

--- conftest.py
+++ conftest.py
@@ -43,22 +43,30 @@ def pytest_generate_tests(metafunc):
         if fixture.startswith("data_groups"):
             test_data = load_from_module_groups(fixture[5:])
             metafunc.parametrize(fixture, test_data, ids=[str(x) for x in test_data])
-        elif fixture.startswith("json_"):
-            test_data = load_from_json(fixture[5:])
+        elif fixture.startswith("json_groups"):
+            test_data = load_from_json_groups(fixture[5:])
             metafunc.parametrize(fixture, test_data, ids=[str(x) for x in test_data])
         elif fixture.startswith("data_contacts"):
             contact_data = load_from_module_contacts(fixture[5:])
             metafunc.parametrize(fixture, contact_data, ids=[repr(x) for x in contact_data])
+        elif fixture.startswith("json_contacts"):
+            contact_date = load_from_json_contacts(fixture[5:])
+            metafunc.parametrize(fixture, contact_date, ids=[str(x) for x in contact_date])
 
 
 def load_from_module_groups(module):
     return importlib.import_module("data.%s" % module).test_data
 
 
-def load_from_json(file):
+def load_from_json_groups(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
 
 
 def load_from_module_contacts(module):
     return importlib.import_module("data.%s" % module).contact_data
+
+
+def load_from_json_contacts(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
