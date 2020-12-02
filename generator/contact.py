4e7c054f35102e4c30a997e1f9aa7738d2f5e5ca
fox140 <malcev223@gmail.com>
2020-12-02 15:24:19 +0300

    Созданн генератор тестовых данных.

--- generator/contact.py
+++ generator/contact.py
@@ -1,6 +1,26 @@
 from model.contact import NewContact
 import random
 import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number for contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 3
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 
 def random_string(prefix, maxlen):
@@ -33,4 +53,10 @@ contact_date = [NewContact(first_name=random_string("First", 10), last_name=rand
                            homepage=random_email(8), bday="15", bmonth="May", byear="1978", aday="25", amonth="June",
                            ayear="2007", address2=random_symbols("Lenina", 20), phone2=random_phone(11),
                            notes=random_symbols("Notes", 20))
-                for i in range(2)]
+                for i in range(n)]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("simplejson", indent=2)
+    out.write(jsonpickle.encode(contact_date))
