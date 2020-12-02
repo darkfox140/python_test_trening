9a52f426ee1bd95696247168b4753dfc69bbc0a0
fox140 <malcev223@gmail.com>
2020-12-02 15:27:21 +0300

    Реализованна функция pytest_generate_tests для групп и контактов.

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,8 +1,18 @@
 from model.contact import NewContact
 
 
-def test_add_new_contact(app, data_contacts):
+'''def test_add_new_contact(app, data_contacts):
     contact = data_contacts
+    old_contact = app.contact.get_contact_list()
+    app.contact.create_new_contact(contact)
+    assert len(old_contact) + 1 == app.contact.count()
+    new_contact = app.contact.get_contact_list()
+    old_contact.append(contact)
+    assert sorted(old_contact, key=NewContact.id_or_max) == sorted(new_contact, key=NewContact.id_or_max)'''
+
+
+def test_add_new_contact(app, json_contacts):
+    contact = json_contacts
     old_contact = app.contact.get_contact_list()
     app.contact.create_new_contact(contact)
     assert len(old_contact) + 1 == app.contact.count()
