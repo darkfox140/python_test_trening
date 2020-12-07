596e2647ac8ce3c6e469e01bac23435de4010c2b
fox140 <malcev223@gmail.com>
2020-12-07 20:57:17 +0300

    Реализованны отключаемые проверки пользовательского интерфейса.

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,8 +1,30 @@
 from model.contact import NewContact
-from random import randrange
+import random
 
 
-def test_delete_some_contact(app):
+def test_delete_some_contact(app, db, check_ui):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(NewContact(first_name="Victor", middle_name="Ivan", last_name="Petrov",
+                                              nick_name="Fox140", tittle="test", company="testcompany", address1="Moscow",
+                                              home_phone="8(495)2154536", mobile_phone="+79168956384",
+                                              work_phone="8-499-9576135", fax="No", email1="testemail1@facegmail.com",
+                                              email2="testemail2@facegmail.com", email3="testemail3@facegmail.com",
+                                              homepage="testhomepage.ru", bday="14", bmonth="August", byear="1987",
+                                              aday="24", amonth="October", ayear="2014", address2="Moscow, Lenina 2",
+                                              phone2="8 456 9535956", notes="Test Moscow information"))
+    old_contact = db.get_contact_list()
+    contact = random.choice(old_contact)
+    app.contact.delete_contact_by_id(contact.id)
+    new_contact = db.get_contact_list()
+    assert len(old_contact) - 1 == len(new_contact)
+    old_contact.remove(contact)
+    assert old_contact == new_contact
+    if check_ui:
+        assert sorted(new_contact, key=NewContact.id_or_max) == \
+               sorted(app.contact.get_contact_list(), key=NewContact.id_or_max)
+
+
+'''def test_delete_some_contact(app):
     if app.contact.count() == 0:
         app.contact.create_new_contact(NewContact(first_name="Victor", middle_name="Ivan", last_name="Petrov",
                                               nick_name="Fox140", tittle="test", company="testcompany", address1="Moscow",
@@ -18,4 +40,4 @@ def test_delete_some_contact(app):
     new_contact = app.contact.get_contact_list()
     assert len(old_contact) - 1 == len(new_contact)
     old_contact[index:index+1] = []
-    assert old_contact == new_contact
+    assert old_contact == new_contact'''
