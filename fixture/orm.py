38efbd3abf1b39b2fb61b28ffbcb12f800975a09
fox140 <malcev223@gmail.com>
2020-12-07 18:22:18 +0300

    Добавленн новый класс и методы(Object Relational Mapping).

---
+++ fixture/orm.py
@@ -0,0 +1,67 @@
+from pony.orm import *
+from datetime import datetime
+from model.group import Group
+from model.contact import NewContact
+
+
+class ORMFixture:
+
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        firstname = Optional(str, column='firstname')
+        middlename = Optional(str, column='middlename')
+        lastname = Optional(str, column='lastname')
+        nickname = Optional(str, column='nickname')
+        company = Optional(str, column='company')
+        title = Optional(str, column='title')
+        address = Optional(str, column='address')
+        home = Optional(str, column='home')
+        mobile = Optional(str, column='mobile')
+        work = Optional(str, column='work')
+        fax = Optional(str, column='fax')
+        email = Optional(str, column='email')
+        email2 = Optional(str, column='email2')
+        email3 = Optional(str, column='email3')
+        homepage = Optional(str, column='homepage')
+        address2 = Optional(str, column='address2')
+        phone2 = Optional(str, column='phone2')
+        notes = Optional(str, column='notes')
+
+    def __init__(self, host, name, user, password):
+        self.db.bind('mysql', host=host, database=name, user=user, password=password)
+        self.db.generate_mapping()
+        sql_debug(True)
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+        return list(map(convert, groups))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return NewContact(id=str(contact.id), first_name=contact.firstname, middle_name=contact.middlename,
+                              last_name=contact.lastname, nick_name=contact.nickname, company=contact.company,
+                              tittle=contact.title, address1=contact.address, home_phone=contact.home,
+                              mobile_phone=contact.mobile, work_phone=contact.work, fax=contact.fax,
+                              email1=contact.email, email2=contact.email2, email3=contact.email3,
+                              homepage=contact.homepage, address2=contact.address2, phone2=contact.phone2,
+                              notes=contact.notes)
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_group_list(self):
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact))
