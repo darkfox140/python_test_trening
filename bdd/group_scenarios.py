2dfb34e70f444a24d042aa5af10da6d0a4d0642b
fox140 <malcev223@gmail.com>
2021-02-01 23:50:56 +0300

    Добавленны новые тесты.

--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -5,3 +5,8 @@ from .group_steps import *
 @scenario('groups.feature', 'Add new group')
 def test_add_new_group():
     pass
+
+
+@scenario('groups.feature', 'Delete a gruop')
+def test_dlete_group():
+    pass
