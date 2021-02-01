6e39357286997320de82230e1aa3dabb9b852e46
fox140 <malcev223@gmail.com>
2021-02-01 23:50:43 +0300

    Добавленны новые фикстуры.

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,5 +1,6 @@
 from pytest_bdd import given, when, then
 from model.group import Group
+import random
 
 
 @given('a group list', target_fixture="group_list")
@@ -23,3 +24,31 @@ def verify_group_added(db, group_list, new_group):
     new_groups = db.get_group_list()
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+@given('a non empty group list', target_fixture="non_empty_group_list")
+def non_empty_group_list(db, app):
+    if len(db.get_group_list()) == 0:
+        app.group.create_group(Group(name="test name"))
+    return db.get_group_list()
+
+
+@given('a random group from the list', target_fixture="random_group")
+def random_group(non_empty_group_list):
+    return random.choice(non_empty_group_list)
+
+
+@when('I delete the group from the list')
+def delete_group(app, random_group):
+    app.group.delete_group_by_id(random_group.id)
+
+
+@then('the new group list is equal to the old list without the deleted group')
+def verify_group_added(app, db, non_empty_group_list, random_group, check_ui):
+    old_groups = non_empty_group_list
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(random_group)
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
